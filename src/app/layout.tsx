e8b836246c9511d01d7cece8f00e987eab6e0c60
kp_pfc <krittayos.p@perfectcomsolutions.com>
2025-01-16 03:21:49 +0700

    update: icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,12 +14,21 @@ export default function RootLayout({
 }) {
    return (
       <html lang='th'>
-         <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
-<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-<link rel="shortcut icon" href="/favicon.ico" />
-<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-<meta name="apple-mobile-web-app-title" content="คิดถึงลูน่า" />
-<link rel="manifest" href="/site.webmanifest" />
+         <link
+            rel='icon'
+            type='image/png'
+            href='/favicon-96x96.png'
+            sizes='96x96'
+         />
+         <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+         <link rel='shortcut icon' href='/favicon.ico' />
+         <link
+            rel='apple-touch-icon'
+            sizes='180x180'
+            href='/apple-touch-icon.png'
+         />
+         <meta name='apple-mobile-web-app-title' content='คิดถึงลูน่า' />
+         <link rel='manifest' href='/site.webmanifest' />
          {/* visit https://realfavicongenerator.net/ to generate favicon for all device and place code below*/}
          <body className='m-0 min-h-screen bg-[#FECFE7] flex flex-col'>
             <nav className='px-4 h-14 bg-white/50 fixed w-full top-0 z-10 backdrop-filter backdrop-blur-sm flex justify-between gap-4 text-primary items-center'>
