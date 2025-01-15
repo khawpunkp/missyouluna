ded9782e6bc3e259f0d86201209ca0219cf678af
kp_pfc <krittayos.p@perfectcomsolutions.com>
2025-01-16 03:28:58 +0700

    update: navbar home link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,9 @@ export default function RootLayout({
          {/* visit https://realfavicongenerator.net/ to generate favicon for all device and place code below*/}
          <body className='m-0 min-h-screen bg-[#FECFE7] flex flex-col'>
             <nav className='px-4 h-14 bg-white/50 fixed w-full top-0 z-10 backdrop-filter backdrop-blur-sm flex justify-between gap-4 text-primary items-center'>
-               <p className='text-3xl'>LTX022</p>
+               <Link className='text-3xl' href={'/'}>
+                  LTX022
+               </Link>
                <div className='flex gap-4 text-lg mobile:text-base'>
                   <Link href={'/'}>คิดถึงลูน่า</Link>
                   <Link href={'/card-list'}>การ์ดของลูน่า</Link>
